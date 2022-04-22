7160f4cad5cd36f68f6b7f82bfe6d5118f6e92f8
tangbaibai <tangguojin1996@163.com>
2022-04-23 05:39:00 +0800

    normalization

---
+++ norm_para.py
@@ -0,0 +1,52 @@
+import os
+from PIL import Image
+import torch
+from torchvision.transforms import functional as F
+
+class UnlabeledDataset(torch.utils.data.Dataset):
+    def __init__(self, root, transform):
+        r"""
+        Args:
+            root: Location of the dataset folder, usually it is /unlabeled
+            transform: the transform you want to applied to the images.
+        """
+        self.transform = transform
+        self.image_dir = root
+        self.filelist = os.listdir(self.image_dir)
+        self.num_images = len(self.filelist)
+
+    def __len__(self):
+        return self.num_images
+
+    def __getitem__(self, idx):
+        # the idx of unlabeled image is not consecutive
+        with open(os.path.join(self.image_dir, self.filelist[idx]), 'rb') as f:
+            img = Image.open(f).convert('RGB')
+
+        return F.to_tensor(img)
+
+def main():
+    #device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    unlabeled_data = UnlabeledDataset(root = '/unlabeled_224', transform = None)
+    data_loader = torch.utils.data.DataLoader(unlabeled_data, batch_size=10,  num_workers=2)
+
+    mean = 0.
+    std = 0.
+    nb_samples = 0.
+    for data in data_loader:
+        #data: batch x 3 x 244 x 244// not using the collate fn by prof 
+        batch_samples = data.size(0)
+        data = data.view(batch_samples, data.size(1), -1)
+        mean += data.mean(2).sum(0)
+        std += data.std(2).sum(0)
+        nb_samples += batch_samples
+
+    mean /= nb_samples
+    std /= nb_samples
+
+
+    torch.save(['mean: ', mean, 'std: ', std],'mean_std.pt')
+
+if __name__ == "__main__":
+    main()
