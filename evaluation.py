2f45dde3a4a6cda22bc1fc4a1763743138ee7a52
Deep Mehta <deepme987@gmail.com>
2022-04-24 12:34:59 -0400

    added eval script

--- evaluation.py
+++ evaluation.py
@@ -49,7 +49,7 @@ def get_transform(train):
         transforms.append(T.RandomHorizontalFlip(0.5))
     return T.Compose(transforms)
 
-def get_model(num_classes, swav=False):
+def get_model(num_classes, swav=False, eval=False):
     # model = torch.hub.load("facebookresearch/swav", "resnet50")
     model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False)
     if swav:
@@ -59,38 +59,24 @@ def get_model(num_classes, swav=False):
     # replace the pre-trained head with a new one
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
+    if eval:
+        model.load_state_dict(torch.load("model-8.pth"))
+        model.eval()
+
     return model
 
 def main():
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     num_classes = 100
-    train_dataset = LabeledDataset(root='/labeled', split="training", transforms=get_transform(train=True))
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=2, collate_fn=utils.collate_fn)
 
     valid_dataset = LabeledDataset(root='/labeled', split="validation", transforms=get_transform(train=False))
     valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=2, shuffle=False, num_workers=2, collate_fn=utils.collate_fn)
 
-    model = get_model(num_classes, True)
+    model = get_model(num_classes, swav=True, eval=True)
     model.to(device)
 
-    params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
-
-    num_epochs = 10
-
-    for epoch in range(num_epochs):
-        # train for one epoch, printing every 10 iterations
-        train_one_epoch(model, optimizer, train_loader, device, epoch, print_freq=10)
-        # update the learning rate
-        lr_scheduler.step()
-        # evaluate on the test dataset
-        evaluate(model, valid_loader, device=device)
-
-        torch.save(model.state_dict(), f"model-{epoch}.pth")
-
-    print("That's it!")
+    evaluate(model, valid_loader, device=device)
 
 if __name__ == "__main__":
     main()
