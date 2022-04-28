5baa3976c72f3e6a300b7c13a936e310bb95c768
tangbaibai <tangguojin1996@163.com>
2022-04-29 04:04:39 +0800

    dp

--- swav_dp/swav_dp.py
+++ swav_dp/swav_dp.py
@@ -383,13 +383,13 @@ def distributed_sinkhorn(out):
 
     # make the matrix sums to 1
     sum_Q = torch.sum(Q)
-    dist.all_reduce(sum_Q)
+    # dist.all_reduce(sum_Q)
     Q /= sum_Q
 
     for it in range(args.sinkhorn_iterations):
         # normalize each row: total weight per prototype must be 1/K
         sum_of_rows = torch.sum(Q, dim=1, keepdim=True)
-        dist.all_reduce(sum_of_rows)
+        # dist.all_reduce(sum_of_rows)
         Q /= sum_of_rows
         Q /= K
 
