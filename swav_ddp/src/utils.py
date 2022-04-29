b3cbef5f212d7a66a34b740d02a9c297ee9ea5cf
tangbaibai <tangguojin1996@163.com>
2022-04-29 08:03:10 +0800

    dp

--- swav_ddp/src/utils.py
+++ swav_ddp/src/utils.py
@@ -71,7 +71,7 @@ def init_distributed_mode(args):
         init_method=args.dist_url,
         world_size=args.world_size,
         rank=args.rank,
-        timeout = timedelta(seconds = 600)
+        timeout = timedelta(seconds = 300)
     )
     print('ini success')
 
