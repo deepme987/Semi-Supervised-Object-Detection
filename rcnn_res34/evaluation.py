5f7a90b6f61b62fd7cc1a88568a6ae5d9da367cd
zhengtr <zhengtr@gmail.com>
2022-05-08 16:02:10 -0400

    update

---
+++ rcnn_res34/evaluation.py
@@ -0,0 +1,82 @@
+# Feel free to modifiy this file.
+# It will only be used to verify the settings are correct
+# modified from https://pytorch.org/docs/stable/data.html
+
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+
+import transforms as T
+import utils
+from engine import train_one_epoch, evaluate
+
+from collections import OrderedDict
+from dataset import UnlabeledDataset, LabeledDataset
+
+# Rebuilt SwAV as the default model doesn't like to run correctly
+# class SwAV(nn.Module):
+#     def __init__(self):
+#         super(SwAV, self).__init__()
+#         self.model = torch.hub.load("facebookresearch/swav:main", "resnet50")
+#         self.model.avgpool = nn.Identity()
+#         self.model.fc = nn.Identity()
+        
+    
+#     def forward(self, x):
+#         res = OrderedDict()
+#         x = self.model.conv1(x)
+#         x = self.model.bn1(x)
+#         x = self.model.relu(x)
+#         x = self.model.maxpool(x)
+#         x = self.model.layer1(x)
+#         res["0"] = x
+#         x = self.model.layer2(x)
+#         res["1"] = x
+#         x = self.model.layer3(x)
+#         res["2"] = x
+#         x = self.model.layer4(x)
+#         res["3"] = x
+#         return res
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.ToTensor())
+    if train:
+        transforms.append(T.RandomHorizontalFlip(0.5))
+    return T.Compose(transforms)
+
+def get_model(num_classes, swav=False, eval=False):
+    # model = torch.hub.load("facebookresearch/swav", "resnet50")
+    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=False)
+    if swav:
+        model.backbone.body = SwAV()
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    if eval:
+        model.load_state_dict(torch.load("model-8.pth"))
+        model.eval()
+
+    return model
+
+def main():
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    num_classes = 100
+
+    valid_dataset = LabeledDataset(root='/labeled', split="validation", transforms=get_transform(train=False))
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=2, shuffle=False, num_workers=2, collate_fn=utils.collate_fn)
+
+    model = get_model(num_classes, swav=True, eval=True)
+    model.to(device)
+
+    evaluate(model, valid_loader, device=device)
+
+if __name__ == "__main__":
+    main()
--- rcnn_res50_2/train_submission.py
+++ rcnn_res50_2/train_submission.py
@@ -24,15 +24,14 @@ parser = argparse.ArgumentParser(description="Evaluate models: Fine-tuning with 
 #########################
 #### swav parameters ####
 #########################
-parser.add_argument("--pretrained_hub", default=0, type=int,
-                    help="if backbone downloaded from Facebook hub")
-parser.add_argument("--swav_file", type=str, default="swav_res50_ep100.pth",
+
+parser.add_argument("--swav_file", type=str, default="swav_res18_ep84.pth",
                     help="path to swav checkpoints")
 parser.add_argument("--hidden_mlp", default=2048, type=int,
                     help="hidden layer dimension in projection head")
 parser.add_argument("--feat_dim", default=128, type=int,
                     help="feature dimension")
-parser.add_argument("--nmb_prototypes", default=2000, type=int,
+parser.add_argument("--nmb_prototypes", default=1000, type=int,
                     help="number of prototypes")
 
 #############################
@@ -40,33 +39,29 @@ parser.add_argument("--nmb_prototypes", default=2000, type=int,
 #############################
 parser.add_argument("--mode", choices=['train', 'eval', 'resume'], 
                     default='train', type=str, help="Choose action.") #TODO: add resume impl
-parser.add_argument("--epochs", default=40, type=int,
+parser.add_argument("--epochs", default=25, type=int,
                     help="number of total epochs to run")
 parser.add_argument("--eval_freq", default=2, type=int,
                     help="Eval the model periodically")
 parser.add_argument("--checkpoint_freq", type=int, default=2,
                     help="Save the model periodically")
-parser.add_argument("--arch", choices=['resnet50', 'resnet18', 'resnet34'],
-                    default='resnet50', type=str, help="Architecture")
+# parser.add_argument("--arch", choices=['resnet50', 'resnet18', 'resnet34'],
+#                     default='resnet18', type=str, help="Architecture")
 parser.add_argument("--high_lr", default=5e-3, type=float,
                     help="lr for rcnn")
-parser.add_argument("--low_lr", default=5e-5, type=float,
+parser.add_argument("--low_lr", default=1e-4, type=float,
                     help="lr for transfer layer in backbone")
-parser.add_argument("--super_low_lr", default=5e-7, type=float,
+parser.add_argument("--super_low_lr", default=1e-6, type=float,
                     help="lr for first block in backbone")
 parser.add_argument("--sched_step", default=4, type=int,
                     help="Step size of lr scheduler")
-parser.add_argument("--sched_gamma", default=0.2, type=float,
+parser.add_argument("--sched_gamma", default=0.1, type=float,
                     help="lr scheduler")
-parser.add_argument("--norm_layer", choices=['FBN', 'GN', 'IN', 'BN'],
-                    default='FBN', type=str, help="Norm layer type.")
-parser.add_argument("--box_head", choices=['mlp', 'res'],
-                    default='mlp', type=str, help="box head type.")
+
 ##########################
 #### other parameters ####
 ##########################
-parser.add_argument("--workers", default=2, type=int,
-                    help="number of data loading workers")
+
 parser.add_argument("--data_path", type=str, default="../../data/labeled",
                     help="path to imagenet")
 parser.add_argument("--checkpoint_path", type=str, default="./back_up",
@@ -88,15 +83,8 @@ def get_transform(train):
     return T.Compose(transforms)
 
 def load_pretrained_swav(ckp_path):
-    if (args.arch == "resnet18"):
-        template = resnet_models.resnet18
-    elif (args.arch == "resnet34"):
-        template = resnet_models.resnet34
-    elif (args.arch == "resnet50"):
-        template = resnet_models.resnet50
-    else:
-        print("??")
-        raise Exception
+    template = resnet_models.resnet18
+
     model = template(
         normalize=True,
         hidden_mlp=args.hidden_mlp, 
@@ -132,17 +120,13 @@ def get_model(num_classes, returned_layers=None):
         model.eval()
         return model
 
-    if args.pretrained_hub == 1:
-        backbone = torch.hub.load("facebookresearch/swav", "resnet50")
-        print("Load pretrained swav backbone from Facebook hub")
-    else:
-        backbone = load_pretrained_swav(args.swav_file)
-        backbone.padding = nn.Identity() #TODO: need to double check this
-        backbone.projection_head = nn.Identity()
-        backbone.prototypes = nn.Identity()
-        print("Load pretrained swav backbone from OUR checkpoint")
+    backbone = load_pretrained_swav(args.swav_file)
+    backbone.padding = nn.Identity()
+    backbone.projection_head = nn.Identity()
+    backbone.prototypes = nn.Identity()
     backbone.avgpool = nn.Identity()
     backbone.fc = nn.Identity()
+    print("Load pretrained swav backbone from OUR checkpoint")
     # --------------------Map resnet backbone---------------------
     if returned_layers is None:
         returned_layers = [1, 2, 3, 4]
@@ -174,20 +158,8 @@ def get_model(num_classes, returned_layers=None):
 
     new_back_bone_copy = copy.deepcopy(new_back_bone)
     model.backbone.body = new_back_bone
-    if args.norm_layer == "FBN":
-        build_model.replace_bn_FBN(model.backbone.body, "backbone.body")
-        model.backbone.body.load_state_dict(new_back_bone_copy.state_dict())
-    elif args.norm_layer == "GN":
-        build_model.replace_bn_GN(model.backbone.body, "backbone.body")
-    elif args.norm_layer == "IN":
-        build_model.replace_bn_IN(model.backbone.body, "backbone.body")
-    elif args.norm_layer == "BN":
-        # build_model.replace_bn_IN(model.backbone.body, "backbone.body")
-        pass
-    else:
-        print("WARNING: Norm Layer not replaced")
-
-    
+    build_model.replace_bn_FBN(model.backbone.body, "backbone.body")
+    model.backbone.body.load_state_dict(new_back_bone_copy.state_dict())
 
     for m in model.parameters():
         m.requires_grad = True
@@ -196,31 +168,28 @@ def get_model(num_classes, returned_layers=None):
     # Change fpn
     # ---------------------------------------------------------
     # ResNet-18
-    if args.arch == "resnet18" or args.arch == "resnet34":
-        model.backbone.fpn.inner_blocks[0] = nn.Conv2d(64, 256, kernel_size=(1, 1), stride=(1, 1))
-        model.backbone.fpn.inner_blocks[1] = nn.Conv2d(128, 256, kernel_size=(1, 1), stride=(1, 1))
-        model.backbone.fpn.inner_blocks[2] = nn.Conv2d(256, 256, kernel_size=(1, 1), stride=(1, 1))
-        model.backbone.fpn.inner_blocks[3] = nn.Conv2d(512, 256, kernel_size=(1, 1), stride=(1, 1))
+    model.backbone.fpn.inner_blocks[0] = nn.Conv2d(64, 256, kernel_size=(1, 1), stride=(1, 1))
+    model.backbone.fpn.inner_blocks[1] = nn.Conv2d(128, 256, kernel_size=(1, 1), stride=(1, 1))
+    model.backbone.fpn.inner_blocks[2] = nn.Conv2d(256, 256, kernel_size=(1, 1), stride=(1, 1))
+    model.backbone.fpn.inner_blocks[3] = nn.Conv2d(512, 256, kernel_size=(1, 1), stride=(1, 1))
 
     # ---------------------------------------------------------
     # Change box_head
     # ---------------------------------------------------------
-    if args.box_head == "mlp":
-        out_channels = model.backbone.out_channels
-        resolution = model.roi_heads.box_roi_pool.output_size[0]
-        representation_size = 1024
-        new_box_head = my_nn.CustomizedBoxHead(out_channels * resolution ** 2, representation_size)
-    elif args.box_head == "res":
-        new_box_head = my_nn.ResBoxHead()
+    out_channels = model.backbone.out_channels
+    resolution = model.roi_heads.box_roi_pool.output_size[0]
+    representation_size = 1024
+    new_box_head = my_nn.CustomizedBoxHead(out_channels * resolution ** 2, representation_size)
+
     model.roi_heads.box_head = new_box_head
 
-    if args.debug == 1:
-        for name, param in model.named_parameters():
-            if param.requires_grad:
-                print(f'TRAIN: {name}')
-            else:
-                print(f'FROZEN: {name}')
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            print(f'TRAIN: {name}')
+        else:
+            print(f'FROZEN: {name}')
 
+    if args.debug == 1:
         for name, child in model.named_children():
             print(f'name is: {name}')
             print(f'module is: {child}')
@@ -228,6 +197,7 @@ def get_model(num_classes, returned_layers=None):
         print(model.backbone.body.layer4[0].bn1.weight)
         print("DONE")
         sys.stdout.flush()
+
     return model
 
 def main():
@@ -275,15 +245,15 @@ def main():
     if args.debug == 1:
         index = list(range(10))
         train_dataset = torch.utils.data.Subset(train_dataset, index)
-        valid_dataset = torch.utils.data.Subset(valid_dataset, index)
+        # valid_dataset = torch.utils.data.Subset(valid_dataset, index)
 
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=2, shuffle=True, num_workers=2, collate_fn=utils.collate_fn)
     valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=2, shuffle=False, num_workers=2, collate_fn=utils.collate_fn)
     
     model = get_model(num_classes)
     model.to(device)
-    if args.gcp_sucks == 1:
-        print(model)
+
+    print(model)
 
     super_low_param = []
     super_low_name = []
@@ -291,25 +261,16 @@ def main():
     low_name = []
     high_lr_param = []
     high_name = []
+
+    # for name, m in model.named_modules():
+    #     if "backbone.body" in name:
+    #         if "bn" in name or "backbone.body.bn" in name:
+    #             m.running_var.fill_(1)
+    #             m.running_mean.zero_()
+
     for name, param in model.named_parameters():
         if "backbone.body" in name:
-            if "bn" in name:
-                if args.norm_layer == "FBN":
-                    pass
-                    # print(f'reset running stats for {name}')
-                    # param.running_var.fill_(1)
-                    # param.running_mean.zero_()
-                elif args.norm_layer == "BN":
-                    if "layer1" in name or "backbone.body.bn" in name:
-                        super_low_param.append(param)
-                        super_low_name.append(name)
-                    else:
-                        low_lr_param.append(param)
-                        low_name.append(name)
-                else:
-                    low_lr_param.append(param)
-                    low_name.append(name)
-            elif "layer1" in name or "backbone.body.conv1" in name:
+            if "layer1" in name or "backbone.body.conv1" in name:
                 super_low_param.append(param)
                 super_low_name.append(name)
             else:
@@ -318,6 +279,7 @@ def main():
         else:
             high_lr_param.append(param)
             high_name.append(name)
+
 
     optimizer = torch.optim.SGD(
          [
@@ -334,12 +296,11 @@ def main():
     print(f'{super_low_name=}')
     for param_group in optimizer.param_groups:
         print(param_group['lr'])
-    # raise NotImplementedError
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.sched_step, gamma=args.sched_gamma)
-    # lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[4, 8, 16], gamma=0.1)
     # optionally resume from a checkpoint
     to_restore = {"epoch": 0}
     if args.mode == 'resume':
+        print("Restart from checkpoint")
         utils.restart_from_checkpoint(
             os.path.join(args.checkpoint_path, "checkpoint.pth.tar"),
             run_variables=to_restore,
@@ -354,7 +315,7 @@ def main():
     if (args.mode == "train") or (args.mode == "resume"):
         for epoch in range(start_epoch, args.epochs):
             # train for one epoch, printing every 10 iterations
-            train_log, smooth_loss_hist = train_one_epoch(model, optimizer, train_loader, device, epoch, print_freq=5000)
+            train_log, smooth_loss_hist = train_one_epoch(model, optimizer, train_loader, device, epoch, print_freq=1000)
             
             if args.gcp_sucks == 0:
                 training_stats_detailed.update(train_log)
@@ -373,18 +334,15 @@ def main():
             # update the learning rate
             lr_scheduler.step()
 
-
             if args.debug == 0:
                 # save whole model instead of state_dict
-                if (((epoch + 1) % args.checkpoint_freq == 0 and (epoch > 0)) or (epoch == args.epochs - 1)):
+                if (((epoch + 1) % args.eval_freq == 0 and (epoch > 0)) or (epoch == args.epochs - 1)):
                     torch.save(model, os.path.join(args.checkpoint_path, f"model_{epoch}.pth"))
 
-                if (((epoch % args.eval_freq) == 0) or (epoch == args.epochs - 1)):
+                if (((epoch + 1) % args.eval_freq == 0 and (epoch > 0)) or (epoch == args.epochs - 1)):
                     # evaluate on the test dataset
                     coco_res, _ = evaluate(model, valid_loader, device=device)
-                    # print(f'coco_res: {coco_res.coco_eval["bbox"].stats}')
                     eval_result[epoch] = coco_res.coco_eval
-                    
 
     # fuck GCP
     # with open(os.path.join(args.checkpoint_path, "eval_res.pickle"), "w") as outfile:
